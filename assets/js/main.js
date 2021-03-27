44422b9277361a5bccb0e183be3f9ab66e871228
ahmedabosaif <64439610+ahmedabosaif@users.noreply.github.com>
2021-03-27 17:53:40 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@
   $('.back-to-top').click(function() {
     $('html, body').animate({
       scrollTop: 0
-    }, 1500, 'easeInOutExpo');
+    }, 250, 'easeInOutExpo');
     return false;
   });
 
