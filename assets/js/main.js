8a3d6200ff95b24af699d4b9be092c2b37506331
ahmedabosaif <64439610+ahmedabosaif@users.noreply.github.com>
2021-03-27 17:52:39 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@
 
         $('html, body').animate({
           scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
+        }, 150, 'easeInOutExpo');
 
         if ($(this).parents('.nav-menu, .mobile-nav').length) {
           $('.nav-menu .active, .mobile-nav .active').removeClass('active');
